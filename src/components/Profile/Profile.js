f4ca5a2bef192c28082f7f6f3807826b3ee5f382
YurikStefniuk <65035267+YurikStefniuk@users.noreply.github.com>
2022-01-27 22:25:40 +0200

    fixed

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Default from '../image/default.PNG';
+import Default from '../../Image/default.PNG';
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
